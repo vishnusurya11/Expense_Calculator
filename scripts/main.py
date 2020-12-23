a89e23caf40ca00ca30fccaee3be1ca04e257952
vishnusurya11 <vishnusurya11@gmail.com>
2020-12-22 23:12:07 -0800

    added get_expense_options function to select options from config

--- scripts/main.py
+++ scripts/main.py
@@ -12,23 +12,30 @@ logging.basicConfig(filename='../log/Error_log.log', level=logging.DEBUG,
                     format='%(asctime)s %(levelname)s %(name)s %(message)s')
 logger=logging.getLogger(__name__)
 
-def get_the_expense_category():
+
+def get_expense_options(keyvalue):
+    try:
+        print('please select a option from below : ')
+        with open('config/expense_details.json') as f:
+            data = json.load(f)
+        for key,value in data[keyvalue].items():
+            print(key + ' : '+ value)
+        return data[keyvalue]
+    except Exception as e:
+        logging.exception(e)
+        return None
+
+def selected_option(keyvalue):
     """
     
     """
     try:
-        print("""
-        Select the Expense Catrgory:
-        a) Rent
-        b) Groceries
-        c) Bills 
-        d) Other
-        """)
-        option_selected = input("select a option : ")
+        options = get_expense_options(keyvalue)
+        option_selected = input("selected a option : ")
+        return options[option_selected]
     except Exception as e:
         logging.exception(e)
-
-    return option_selected
+        return None
 
 
 def write_object_to_file(expense):
@@ -43,9 +50,9 @@ def expense_calculator():
     expense = dict()
     expense["id"] =  int(round(time.time() * 1000)) 
     expense["create_time"] = datetime.now().strftime("%m -%d -%Y  %H:%M:%S PST") 
-    expense["expense_type"] = get_the_expense_category()
+    expense["expense_type"] = selected_option("expense_type")
+    expense["expense_name"] = selected_option("expense_name")
     write_object_to_file(expense)
 
 if __name__ == "__main__":
     expense_calculator()
-    
